0035936c2594f8dc29f00496f4d032420a8594a0
Plasnid <stuart@sackler.ca>
2020-01-21 15:10:44 -0500

    functions intro

--- script.js
+++ script.js
@@ -32,13 +32,26 @@ let isSleeping = true;
 
 
 let pharoahPlants = [
-    {plantName: "basil", colour:"green", needsWatering:true},
-    {plantName: "rosemary", colour:"green", needsWatering:false},
-    {plantName: "sweet millions tomatoes", colour:"orange", needsWatering:true},
-    {plantName: "peppermint", colour:"green", needsWatering:false},
-    {plantName: "cillantro", colour:"green", needsWatering:true},
-    {plantName: "strawberries", colour:"red", needsWatering:true}
+    {plantName: "basil", colour:"green", minMoisture:0.7 ,soilCondition:0.6},
+    {plantName: "rosemary", colour:"green", minMoisture: 0.3, soilCondition:0.6},
+    {plantName: "sweet millions tomatoes", minMoisture: 0.9, colour:"orange", soilCondition:0.8},
+    {plantName: "peppermint", colour:"green", minMoisture: 0.2, soilCondition:0.3},
+    {plantName: "cillantro", colour:"green", minMoisture: 0.8, soilCondition:0.5},
+    {plantName: "strawberries", colour:"red", minMoisture: 0.1, soilCondition:0.09}
 ]
+
+function waterPlants(plant){
+    if(plant.soilCondition<plant.minMoisture){
+        console.log(`${plant.plantName} needs watering`);
+    }else{
+        console.log(`${plant.plantName} doesn't need watering`);
+    }
+}
+
+for(let i=0; i<pharoahPlants.length; i++){
+    waterPlants(pharoahPlants[i]);
+}
+
 
 //number of plants
 let numPlants = pharoahPlants.length;
@@ -50,6 +63,10 @@ console.log(pharoahPlants[pharoahPlants.length - 1]);
 let strawberries = pharoahPlants[pharoahPlants.length - 1];
 console.log(strawberries.needsWatering);
 console.log(strawberries["needsWatering"]);
+
+console.log(1=="1");
+console.log(1==="1");
+console.log(1===1);
 
 
 
